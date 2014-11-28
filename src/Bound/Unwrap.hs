f8fcedb809dc4ee24508866e54e7e33abb390ae9
Danny Gratzer <jozefg@cmu.edu>
2014-11-28 18:30:33 -0500

    Added some run functions for UnwrapT

--- src/Bound/Unwrap.hs
+++ src/Bound/Unwrap.hs
@@ -1,7 +1,14 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE ConstraintKinds #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Bound.Unwrap (Name, name, Counter, UnwrapT, Unwrap) where
+module Bound.Unwrap (Name,
+                     name,
+                     Counter,
+                     UnwrapT,
+                     Unwrap,
+                     runUnwrapT,
+                     runUnwrap,
+                     unwrap) where
 import Bound
 import Control.Applicative
 import Control.Monad.Gen
@@ -20,6 +27,12 @@ newtype Counter = Counter {getCounter :: Int}
 type UnwrapT = GenT Counter
 type Unwrap = Gen Counter
 type MonadUnwrap m = MonadGen Counter m
+
+runUnwrapT :: Monad m => GenT Counter m a -> m a
+runUnwrapT = runGenTWith (successor $ Counter . succ . getCounter) (Counter 0)
+
+runUnwrap :: Gen Counter a -> a
+runUnwrap = runGenWith (successor $ Counter . succ . getCounter) (Counter 0)
 
 freshify :: (MonadUnwrap m, Functor m) => Name a -> m (Name a)
 freshify nm = (\i -> nm{fresh = i}) <$> fmap getCounter gen
